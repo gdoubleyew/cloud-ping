5a63caf9318bd8e748b0b3314dc4f027ea2a0894
Grant Wallace <gwallace@princeton.eDU>
2017-11-28 12:46:15 -0500

    Add server side functionality

--- cloudping.py
+++ cloudping.py
@@ -11,23 +11,36 @@ uses.
 import sys
 import getopt
 import logging
+import socketserver
 
-# TODO - use SocketServer framework to make connections.
-# https://docs.python.org/2/library/socketserver.html
+# TODO - use socketserver framework to make connections.
+# https://docs.python.org/3/library/socketserver.html
 
 # GetOpt - http://www.diveintopython.net/scripts_and_streams/command_line_arguments.html
 # https://docs.python.org/3.1/library/getopt.html
 
+SERVER_MODE = "server"
+CLIENT_MODE = "client"
+
 class InvalidInvocation(ValueError):
     pass
+
+class InvalidMode(ValueError):
+    pass
+
+class ServerRequestHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        self.data = self.request.recv(1024).strip()
+        print("received: ({}), from {}".format(self.data, self.client_address[0]))
+        self.request.sendall(self.data)
 
 class CloudPing():
     def __init__(self, argv):
         self.argv = argv
-        self.mode = None # "server"
-        self.addr = None # "127.0.0.1"
-        self.port = "0" # "5500"
-
+        self.mode = SERVER_MODE
+        self.addr = "127.0.0.1"
+        self.port = 5500
+        self.parseArgs()
 
     def printUsage(self):
         usage_format = """Usage:
@@ -43,7 +56,7 @@ class CloudPing():
         # parse args
         try:
             opts, _ = getopt.gnu_getopt(self.argv[1:], "a:p:cs",
-                                        ["addr=", "port=", "client", "server"])
+                                        ["addr=", "port=", CLIENT_MODE, SERVER_MODE])
         except getopt.GetoptError as err:
             logging.error(repr(err))
             raise InvalidInvocation("Invalid parameter specified")
@@ -52,25 +65,39 @@ class CloudPing():
             if opt in ("-a", "--addr"):
                 self.addr = arg
             elif opt in ("-p", "--port"):
-                self.port = arg
+                self.port = int(arg)
             elif opt in ("-c", "--client"):
-                self.mode = "client"
+                self.mode = CLIENT_MODE
             elif opt in ("-s", "--server"):
-                self.mode = "server"
+                self.mode = SERVER_MODE
             else:
                 raise InvalidInvocation("unimplemented option {} {}", opt, arg)
 
         if self.mode is None:
             raise InvalidInvocation("Must specify either client -c or server -s mode")
 
+    def listen(self):
+        if self.mode != SERVER_MODE:
+            raise InvalidMode("listen() method can only be called in server mode")
+        logging.info("Server mode: listening on %s:%s", self.addr, self.port)
+        server = socketserver.TCPServer((self.addr, self.port), ServerRequestHandler)
+        server.serve_forever()
+
+
 def main(argv):
-    cloudping = CloudPing(argv)
+    logging.basicConfig(level=logging.INFO)
     try:
-        cloudping.parseArgs()
+        cloudping = CloudPing(argv)
+        if cloudping.mode == SERVER_MODE:
+            cloudping.listen()
+        elif cloudping.mode == CLIENT_MODE:
+            # TODO - implement client side
+            pass
     except ValueError as err:
         logging.error(repr(err))
         cloudping.printUsage()
-
+    except KeyboardInterrupt:
+        print() # newline
 
 if __name__ == "__main__":
     main(sys.argv)
--- test_cloudping.py
+++ test_cloudping.py
@@ -1,5 +1,12 @@
+#!/user/bin/env python3
+
 import unittest
+import subprocess
+import threading
 import cloudping as cp
+
+TEST_ADDR = '1.1.1.1'
+TEST_PORT = 5500
 
 class TestParseOptions(unittest.TestCase):
     def setUp(self):
@@ -9,32 +16,46 @@ class TestParseOptions(unittest.TestCase):
         pass
 
     def test_clientMode(self):
-        cloudping = cp.CloudPing(['program.py', '-c', '-a', '1.1.1.1', '-p', '5500'])
-        cloudping.parseArgs()
+        # test short form options
+        cloudping = cp.CloudPing(['program.py', '-c', '-a', TEST_ADDR, '-p', str(TEST_PORT)])
         self.assertEqual(cloudping.mode, 'client')
-        self.assertEqual(cloudping.addr, '1.1.1.1')
-        self.assertEqual(cloudping.port, '5500')
-
-        cloudping = cp.CloudPing(['program.py', '--client', '--addr=1.1.1.1', '--port=5500'])
-        cloudping.parseArgs()
+        self.assertEqual(cloudping.addr, TEST_ADDR)
+        self.assertEqual(cloudping.port, TEST_PORT)
+        # test long form options
+        cloudping = cp.CloudPing(['program.py', '--client', '--addr='+TEST_ADDR, '--port='+str(TEST_PORT)])
         self.assertEqual(cloudping.mode, 'client')
-        self.assertEqual(cloudping.addr, '1.1.1.1')
-        self.assertEqual(cloudping.port, '5500')
+        self.assertEqual(cloudping.addr, TEST_ADDR)
+        self.assertEqual(cloudping.port, TEST_PORT)
 
     def test_serverMode(self):
         cloudping = cp.CloudPing(['program.py', '-s', '-p', '5500'])
-        cloudping.parseArgs()
         self.assertEqual(cloudping.mode, 'server')
-        self.assertEqual(cloudping.port, '5500')
-
+        self.assertEqual(cloudping.port, TEST_PORT)
+        # test long form options
         cloudping = cp.CloudPing(['program.py', '--server', '--port=5500'])
-        cloudping.parseArgs()
         self.assertEqual(cloudping.mode, 'server')
-        self.assertEqual(cloudping.port, '5500')
+        self.assertEqual(cloudping.port, TEST_PORT)
 
     def test_invalidArgs(self):
-        cloudping = cp.CloudPing(['program.py', '--NoSuchOption'])
-        self.assertRaises(cp.InvalidInvocation, cloudping.parseArgs)
+        with self.assertRaises(cp.InvalidInvocation):
+            cp.CloudPing(['program.py', '--NoSuchOption'])
+
+
+class TestClientServer(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+    def test_server(self):
+        server = cp.CloudPing(['program.py', '-s'])
+        def listener():
+            server.listen()
+        server_thread = threading.Thread(name='server', target=listener)
+        server_thread.setDaemon(True)
+        server_thread.start()
+        result = subprocess.run("echo hello | netcat localhost 5500", shell=True, stdout=subprocess.PIPE)
+        self.assertEqual(result.stdout.decode('utf-8'), 'hello')
+
 
 if __name__ == '__main__':
     unittest.main()
